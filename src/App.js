15185e5996850365eca6298beb62fa480ed0d26f
poshones <113612705+is-potions@users.noreply.github.com>
2023-07-05 18:37:44 +0800

    implemented search functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c2ecde5d'
 const App = () => {
 
     const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState([])
 
     const searchMovies = async (title) => {
         const response = await (fetch(`${API_URL}&s=${title}`));
@@ -28,14 +29,15 @@ const App = () => {
             <div className="search">
                 <input 
                 placeholder="Search for Movies"
-                value="Superman"
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyDown={() => searchMovies(searchTerm)}
                 />
                 <img
                     src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
-                />    
+                    onClick={() => searchMovies(searchTerm)}
+                />
             </div>
 
             {movies?.length > 0
