e3d5e183b79cf76d227070271a8617e7609c30e0
poshones <113612705+is-potions@users.noreply.github.com>
2023-07-05 16:53:51 +0800

    added dynamic moviecards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import MovieCard from './MovieCard';
+
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c2ecde5d'
 
 const App = () => {
+
+    const [movies, setMovies] = useState([])
+
+    const searchMovies = async (title) => {
+        const response = await (fetch(`${API_URL}&s=${title}`));
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    
+    useEffect(() => {
+        searchMovies('Superman');
+    }, [])
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search for Movies"
+                value="Superman"
+                onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />    
+            </div>
+
+            {movies?.length > 0
+              ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                </div>
+              ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+              )}
+            </div>
     )
 }
 
